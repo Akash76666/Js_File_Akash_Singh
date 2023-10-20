a1cd088f5a3e0d899e95991950ae4bb8a2c861d1
Akash Singh <115129789+Akash76666@users.noreply.github.com>
2023-10-20 06:21:53 +0000

    update

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountNo=134567
+let accountPass=12345
+var accountName="Akash"
+
+//accountNo=1223 //Not allowed
+
+/* Never use var 
+beacause it is not helpful in Block and Functioonal scoping */
+accountPass=13456
+accountName="Manish"
+
+console.table([accountNo,accountName,accountPass])
