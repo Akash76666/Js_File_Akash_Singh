cbdfb0ec49e15a8599464f7c41b1e3936f219b7d
Akash Singh <115129789+Akash76666@users.noreply.github.com>
2023-10-20 06:50:06 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//alert(3+3)  //we are using nodejs not browser
+console.log(3
+    +3) //We can write like that but code readebility in not good 
+console.log(3+4) //code readebility is good in this case
+
+let name="Akash"
+let age=18
+let isLogedIn=false
+
+// Number of dataTypes are
+// bigint
+// Boolean
+//String
+//null => Standalone value
+//undefined
+//symbol => unique
+
+console.log(typeof age)
+
+console.log(typeof null) //null
+
+console.log(typeof undefined)  //undefined
