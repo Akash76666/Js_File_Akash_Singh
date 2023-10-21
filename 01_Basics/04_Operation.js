c10ece68d2d8774e6f46e6995917308379abeae2
Akash Singh <115129789+Akash76666@users.noreply.github.com>
2023-10-21 07:45:41 +0000

    Operation

---
+++ 01_Basics/04_Operation.js
@@ -0,0 +1,28 @@
+console.log(3+3)
+console.log(4-3)
+console.log(2**2)
+console.log(2*2)
+console.log(4%2)
+console.log(2/2)
+
+let str1="Akash"
+let str2=" Singh"
+
+let str3=str1+str2
+console.log(str1+str2)
+
+console.log("1"+2) //If the first value is String them all value treated as String.
+console.log(1+"2")
+console.log("1"+2+2+2)
+
+console.log(2+2+"2") //If the Last value is String all Conversion Before it donw first.
+
+let a=10
+let b=a++ //It return the value before increment.
+
+console.log("Post Increment: ",b)
+
+let x=10
+let y=++x  //It return the value after increment.
+
+console.log(y) 
