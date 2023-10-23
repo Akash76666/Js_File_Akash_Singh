6d62a51e568a71f6588cbae4eb332cee628fb1a2
Akash Singh <115129789+Akash76666@users.noreply.github.com>
2023-10-23 19:37:44 +0000

    memory

---
+++ 01_Basics/Memory_allocation.js
@@ -0,0 +1,27 @@
+// Primitive value is stored in Stack
+// Reference type is stored in heap
+
+/* It generate the copy of value.If you change the another value it is not change 
+in the original value */
+let myName="Akash"
+let Name=myName
+myName="Akhil"
+
+console.log(Name)
+console.log(myName)
+
+/* Reference type gives the refrence og value it does not give the copy 
+if you change the one object value ita also chnage in original value */
+
+let obj1={
+    a:10,
+    b:"Akash"
+}
+
+let obj2=obj1
+obj2.a=20
+obj2.b="Bheem"
+console.log("Reference type value: ",obj1.a)
+console.log("Reference type value: ",obj1.b)
+console.log(obj2.a)
+console.log(obj2.b)
