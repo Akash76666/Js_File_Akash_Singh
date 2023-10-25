3f2370a63b709227c4e6955177465bc97ef87627
Akash Singh <115129789+Akash76666@users.noreply.github.com>
2023-10-25 08:57:47 +0000

    String

--- 01_Basics/06_string.js
+++ 01_Basics/06_string.js
@@ -17,3 +17,28 @@ console.log(str1.__proto__)
 console.log(`The uppercase Methods in String: ${str1.toUpperCase()}`)
 
 console.log(`The CharAt Prorperty in string: ${str1.charAt(5)}`)
+
+console.log(`The Sub-String method in string: ${str1.substring(1,5)}`)
+
+console.log(`The Slice method in String: ${str1.slice(-8,3)}`)
+
+const str2="      Akash Singh       "
+console.log(`The String is: ${str2}`)
+console.log(`The Trim method in String: ${str2.trim()}`)
+console.log(`The Replace method in String: ${str2.replace('S','D')}`)
+console.log(`The Trim-start property: ${str2.trimStart()}`)
+
+const url="http://akashwebsite%20.com/AkashSingh"
+const rep=url.replace('%20','5')
+console.log(rep)
+
+console.log(str.includes("Akash"))
+
+const str3="All the methods of String"
+
+console.log(`The split method is: ${str3.split('-')}`)
+const strcopy=str3.split('',2)  //It gives two letters as Split value [ 'A', 'l' ]
+console.log(strcopy)
+
+const strsplit=str3.split(' ',2)
+console.log(strsplit) //It gives two split words as Split value ['All','the']
