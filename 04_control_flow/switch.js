b10399ef94b7236aa05760a2c48b8a315a51f247
Akash Singh <115129789+Akash76666@users.noreply.github.com>
2023-11-03 17:59:15 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,43 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const month=3
+switch (month) {
+    case 1:
+     console.log("January")
+        break;
+    case 2:
+     console.log("February")
+        break;
+    case 3:
+     console.log("March")
+        break;
+    case 4:
+     console.log("April")
+        break;
+
+    default:
+        console.log("Entered Wrong key")
+        break;
+}
+
+const day="Wednesday"
+ switch (day) {
+    case "Monday":
+        console.log("monday")
+        break;
+        case "Tuesday":
+            console.log("monday")
+            break;
+    case "Wednesday":
+        console.log("Wednesday")
+        break;
+    default:
+        console.log("Day is not declared")
+        break;
+ }
