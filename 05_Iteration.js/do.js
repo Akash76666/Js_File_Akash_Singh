d474c2c38d348eaa117762315676dcd29ee1eb70
Akash Singh <115129789+Akash76666@users.noreply.github.com>
2023-11-04 16:49:59 +0000

    loop

---
+++ 05_Iteration.js/do.js
@@ -0,0 +1,18 @@
+let index=0
+while (index <= 10) {
+    console.log(`Value of index is: ${index}`)
+    index= index +2
+}
+
+let myArray=["Super-Man","Batman","Aquaman","Flash"]
+let arr=0
+while(arr<myArray.length){
+    console.log(`Element of Array is: ${myArray[arr]}`)
+    arr=arr+1
+}
+
+let i=11
+do {
+    i++
+    console.log(`Value of i is: ${i}`)
+} while (i<=10);
--- 05_Iteration.js/one.js
+++ 05_Iteration.js/one.js
@@ -21,3 +21,38 @@ for (let i = 0; i < 5; i++) {
         console.log(`Inner loop j: ${element1} and Inner loop: ${element}`)    
     }
 }
+
+for (let i = 1; i <= 10; i++) {
+    const element = i;
+    console.log("Outer loop i:",element)
+    for (let j = 1; j <= 10; j++) {
+        const element1=j
+        console.log(`${i} * ${j} = ${i*j}`)    
+    }
+}
+
+//        +++++++ Loop on Array   +++++++
+const myArray=["Iron-man","Loki","Spider-Man","Thor","Black-Panther"]
+console.log(`The length of array is: ${myArray.length}`)
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(`The elment ${index} of array: ${element}`)
+    
+}
+
+// Break and Continue in Loop
+for (let index = 1; index <= 20; index++) {
+    if(index===5){
+        console.log(`Detected 5`)
+        break
+    }
+    console.log(index)
+}
+
+for (let index = 1; index <= 20; index++) {
+    if(index===5){
+        console.log(`Detected 5`)
+        continue
+    }
+    console.log(index)
+}
